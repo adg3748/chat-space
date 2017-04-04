991432d5e25156db8e6e2b0ede9282f144d17227
adg3748 <revivedtomorrow@gmail.com>
2017-04-04 13:28:23 +0900

    design group_migration

---
+++ db/migrate/20170404041739_groups.rb
@@ -0,0 +1,9 @@
+class Groups < ActiveRecord::Migration[5.0]
+  def change
+    create_table :groups do |t|
+      t.integer :id
+      t.string :name, null: false
+      t.timestamps
+    end
+  end
+end
