47e23bf4a1eb1f2c8d08db706089795f99f544ea
adg3748 <revivedtomorrow@gmail.com>
2017-04-04 19:01:59 +0900

    delete no use description

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,3 @@ module ChatSpace
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
-
-# config.i18n.default_locale = :ja
-# config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
