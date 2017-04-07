a2f1559aaafb9e8c301df3dbdcedc466a543d323
adg3748 <revivedtomorrow@gmail.com>
2017-04-07 21:37:58 +0900

    make codes easy to read

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,28 @@
 class GroupsController < ApplicationController
+
   def new
+    @group = Group.new
   end
+
   def edit
+    @group = Group.find(params[:id])
   end
+
   def create
     @group = Group.create(name: group_params[:name])
     redirect_to root_path
   end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to root_path
+  end
+
   private
+
   def group_params
     params.require(:chat).permit(:name)
   end
+
 end
