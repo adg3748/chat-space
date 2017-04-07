ac8799756dfc8cfd8274927a850e944fa1151aaf
adg3748 <revivedtomorrow@gmail.com>
2017-04-07 21:33:21 +0900

    redirect to index if user not signed in

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,14 @@
 class MessagesController < ApplicationController
-  
-  before_action :move_to_index, except: :index
+
+  before_action :move_to_index
 
   def index
+    # 一時的にid=1のレコードを代入
+    @group = Group.find(1)
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
 end
