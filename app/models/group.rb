d6a2a3f64cbd30eec1c12cb366c2e7103ff0bcdb
adg3748 <revivedtomorrow@gmail.com>
2017-04-14 20:19:11 +0900

    add some options

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
-  has_many :groups_users, dependent: :destroy
+  has_many :groups_users, class_name: "Groups_users", dependent: :delete_all
   has_many :users, through: :groups_users
   validates :name, presence: true
-  accepts_nested_attributes_for :groups_users
+  accepts_nested_attributes_for :users, allow_destroy: true
 end
