8c3e2ee4624de44ef38ec032a0c48235c88116af
adg3748 <revivedtomorrow@gmail.com>
2017-04-15 19:31:26 +0900

    describe polymorphic_relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ActiveRecord::Base
   has_many :groups_users, class_name: "Groups_users", dependent: :delete_all
   has_many :users, through: :groups_users
+  has_many :messages
   validates :name, presence: true
   accepts_nested_attributes_for :users, allow_destroy: true
 end
