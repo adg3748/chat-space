ae133825a0c03bc715b2fcf22d6847bd55831105
adg3748 <revivedtomorrow@gmail.com>
2017-04-15 19:30:35 +0900

    apply function of sending messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,10 +4,23 @@ class MessagesController < ApplicationController
   def index
     # 一時的にid=1のレコードを代入
     @group = Group.find(1)
+    @message = Message.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @message = Message.create(content: message_params[:content], user_id: current_user.id, group_id: @group.id)
+    redirect_to root_path
   end
 
   def move_to_index
     redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 
 end
