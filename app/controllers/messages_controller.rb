23f792f5b169b44f1afd90a3ced6518127a6bb89
adg3748 <revivedtomorrow@gmail.com>
2017-04-23 17:18:05 +0900

    insert 条件分岐

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,20 +4,27 @@ class MessagesController < ApplicationController
   def index
     # 一時的にid=1のレコードを代入
     @group = Group.find(1)
+    @groups = current_user.groups
     @message = Message.new
+    @messages = @group.messages
   end
 
   def create
     @group = Group.find(params[:group_id])
-    @message = Message.create(content: message_params[:content], user_id: current_user.id, group_id: @group.id)
-    redirect_to root_path
-  end
-
-  def move_to_index
-    redirect_to new_user_session_path unless user_signed_in?
+    @message = Message.new(content: message_params[:content], user_id: current_user.id, group_id: @group.id)
+    if @message.save
+      redirect_to root_url
+    else
+      flash.now[:alert] = "メッセージの送信に失敗しました。"
+      redirect_to root_url
+    end
   end
 
   private
+
+  def move_to_index
+    redirect_to new_user_session_url unless user_signed_in?
+  end
 
   def message_params
     params.require(:message).permit(:content)
