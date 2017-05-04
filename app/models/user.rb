009a4d90349c1463ab9707c8c25d9c249f752c04
adg3748 <revivedtomorrow@gmail.com>
2017-05-04 19:24:29 +0900

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :name, :encrypted_password, :sign_in_count, presence: true
+  #validation
+  validates :name, presence: true, uniqueness: true
+  validates :encrypted_password, :sign_in_count, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 8, too_short: "パスワードの文字数が不足しています" }
+
+  #association
   has_many :groups_users, class_name: "Groups_users"
   has_many :groups, through: :groups_users
   has_many :messages
