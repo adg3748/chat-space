884d5b92586e31465f17f4083a382784a4b9f441
adg3748 <revivedtomorrow@gmail.com>
2017-05-05 12:38:17 +0900

    モデルテストの記述

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+describe Message do
+  describe '#create'
+
+  it "is valid with a content" do
+    message = build(:message)
+    expect(message).to be_valid
+  end
+
+  it "is invalid without a content" do
+    message = build(:message, content: nil)
+    message.valid?
+    expect(message.errors[:content]).to include("メッセージを入力してください")
+  end
+
+end
