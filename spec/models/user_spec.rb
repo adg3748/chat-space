4bab2d16ec08c3d570e0504d3fed5a87faae6b12
adg3748 <revivedtomorrow@gmail.com>
2017-05-05 12:58:55 +0900

    余計な記述削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,18 +38,14 @@ describe User do
     end
 
     it "is invalid with a duplicate name" do
-      #はじめにユーザーを登録
       user = create(:user)
-      #先に登録したユーザーと同じemailの値を持つユーザーのインスタンスを作成
       another_user = build(:user, name: user.name)
       another_user.valid?
       expect(another_user.errors[:name]).to include("が重複しています")
     end
 
     it "is invalid with a duplicate email address" do
-      #はじめにユーザーを登録
       user = create(:user)
-      #先に登録したユーザーと同じemailの値を持つユーザーのインスタンスを作成
       another_user = build(:user, email: user.email)
       another_user.valid?
       expect(another_user.errors[:email]).to include("が重複しています")
