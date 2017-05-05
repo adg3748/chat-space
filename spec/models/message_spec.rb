65955f845cacbb452fdc8a69242fc391f9a475db
adg3748 <revivedtomorrow@gmail.com>
2017-05-05 12:58:37 +0900

    do~endが抜けていたため

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 describe Message do
-  describe '#create'
+  describe '#create' do
 
-  it "is valid with a content" do
-    message = build(:message)
-    expect(message).to be_valid
+    it "is valid with a content" do
+      message = build(:message)
+      expect(message).to be_valid
+    end
+
+    it "is invalid without a content" do
+      message = build(:message, content: nil)
+      message.valid?
+      expect(message.errors[:content]).to include("メッセージを入力してください")
+    end
+    
   end
-
-  it "is invalid without a content" do
-    message = build(:message, content: nil)
-    message.valid?
-    expect(message.errors[:content]).to include("メッセージを入力してください")
-  end
-
 end
