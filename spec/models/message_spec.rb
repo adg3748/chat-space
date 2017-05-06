da381e744a500ac11437185a2b25b7868fe600ff
adg3748 <revivedtomorrow@gmail.com>
2017-05-06 15:41:39 +0900

    モデルテスト追加記述

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -12,6 +12,18 @@ describe Message do
       message.valid?
       expect(message.errors[:content]).to include("メッセージを入力してください")
     end
-    
+
+    it "is invalid without a user_id" do
+      message = build(:message, user_id: nil)
+      message.valid?
+      expect(message.errors[:user_id]).to include("不正な送信です")
+    end
+
+    it "is invalid without a group_id" do
+      message = build(:message, group_id: nil)
+      message.valid?
+      expect(message.errors[:group_id]).to include("不正な送信です")
+    end
+
   end
 end
