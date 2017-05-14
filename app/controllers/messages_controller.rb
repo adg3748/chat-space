7c2d0b421ce20691cf559200274166aee61113db
adg3748 <revivedtomorrow@gmail.com>
2017-05-14 13:51:11 +0900

    controller修正
    includesを1つに統合
    orderをscopeで実装
    validatesで弾いた時の画面遷移停止
    ms_jsonのインデント修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,54 +1,42 @@
 class MessagesController < ApplicationController
-  before_action :move_to_sign_in
-  before_action :get_groups, only: [:index, :show]
+  before_action :get_groups, only: [:index, :show, :create]
 
   def index
   end
 
   def show
-    @group = Group.includes(:users).includes(:messages).find(params[:id])
-    gon.group = @group # Gemのgonを用いてjs内でcontrollerで設定した変数を使えるように
     @message = current_user.messages.new
-    @messages = @group.messages.includes(:user).includes(:group).order("created_at ASC")
+    @group = Group.includes(:users, :messages).find(params[:id])
+    @messages = @group.messages.includes(:user, :group).sort_ASC("created_at")
   end
 
   def create
-    @group = Group.find(params[:group_id])
     @message = current_user.messages.new(message_params)
-    if @message.save!
+    @group = Group.includes(:users, :messages).find(params[:group_id])
+    @messages = @group.messages.includes(:user, :group).sort_ASC("created_at")
+    if @message.save
       respond_to do |format|
         format.html { redirect_to message_url(@group) }
         format.json { render json: message_json(@message) }
       end
     else
       flash.now[:alert] = "メッセージの送信に失敗しました。"
-      redirect_to message_url(@group)
+      render :show
     end
   end
 
   private
-
-  def move_to_sign_in
-    redirect_to new_user_session_url unless user_signed_in?
-  end
 
   def message_params
     params.require(:message).permit(:content).merge(group_id: params[:group_id])
   end
 
   def get_groups
-    @groups = current_user.groups.includes(:users).includes(:messages).order("created_at DESC")
+    @groups = current_user.groups.includes(:users, :messages).sort_DESC("created_at")
   end
 
   def message_json(message)
-    {
-      id: message.id,
-      name: message.user.name,
-      content: message.content,
-      user_id: message.user_id,
-      group_id: message.group_id,
-      created_at: message.created_at.strftime("%Y/%m/%d %H:%M:%S"),
-      updated_at: message.updated_at
+    { id: message.id, name: message.user.name, content: message.content, user_id: message.user_id, group_id: message.group_id, created_at: message.created_at.strftime("%Y/%m/%d %H:%M:%S"), updated_at: message.updated_at
     }
   end
 
