87ab5d8db31832e72556c201cb9dd84aa4b55fb8
adg3748 <revivedtomorrow@gmail.com>
2017-05-14 13:52:40 +0900

    rails余計なレイアウト変更停止
    validatesで弾いた時に余計なdivが作られるのを停止

--- config/application.rb
+++ config/application.rb
@@ -6,5 +6,9 @@ module ChatSpace
   class Application < Rails::Application
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    # validationでレコード作成弾いた時に自動生成される<div class="field_with_errors"></div>を停止させるため
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      %Q(#{html_tag}).html_safe
+    end
   end
 end
